b6e9ead257064a73ae9db9484e8467a43ef409cf
Sohgetsu1123 <ohotarubi@gmail.com>
2023-01-28 03:04:47 +0900

    [Add] index

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import traceback
 
-version = '1.0'
+version = '1.1'
 cur_dir = os.getcwd()
 
 #譜面合成後に出力するファイルの初期化
@@ -11,19 +11,19 @@ with open(f"{cur_dir}/connected.sus", mode='w') as f:
 
 try:
 
+    ext_type = '.sus'
     score_lines = []
 
     #読み取った譜面情報を統合
     for i in range(1, len(sys.argv)):
         file = sys.argv[i]
         ext = os.path.splitext(file)
-        with open(file) as f:
-            lines = f.readlines()
-            #print(f"{lines}\n")
-            score_lines.extend(lines)
-
-    #print(score_lines)
-    #print("\n")
+        if ext_type == ext[1]:
+            with open(file) as f:
+                lines = f.readlines()
+                score_lines.extend(lines)
+        else:
+            raise Exception
 
     #譜面情報の書き込み
     with open(f"{cur_dir}/connected.sus", mode='a') as f:
@@ -31,8 +31,6 @@ try:
         f.writelines(new_score_lines)
 
 except Exception as e:
-    ty, v, tr = sys.exc_info()
-    print(traceback.format_exception(ty, v, tr))
-    print("エラーが発生しました。")
-    input("")
+   traceback.print_exc()
+   input("終了するには何かキーを押してください...")
 
